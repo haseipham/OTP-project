e0be8b8b6085761486700e110f6b7c8d075b2be1
desold <dontask>
2025-09-11 16:51:05 +0700

    one more time :)

--- core/otp.py
+++ core/otp.py
@@ -1,4 +1,9 @@
-# python -m otp instead of python -m otp_generator
+# python -m otp instead of python -m otp_cli
+"""
+Program now no longer: python -m otp init
+Use: python -m otp init --user USERNAME 
+and the --user argument is required for all commands.
+"""
 from otp_cli import (
     main,
 )
